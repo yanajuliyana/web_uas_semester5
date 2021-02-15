1c2cde5e974162900d03fc771de1112c9bf60459
yana juliyana <yanajuliyana23@gmail.com>
2021-02-15 23:50:55 +0700

    edit po

--- admin/po-terima.php
+++ admin/po-terima.php
@@ -79,7 +79,7 @@ if (isset($_SESSION['start_time'])) {
     $elapsed_time = time() - $_SESSION['start_time'];
     if ($elapsed_time >= $timeout) {
         session_destroy();
-        echo "<script>alert('Session Anda Telah Habis!'); window.location = '$logout_redirect_url'</script>";
+        echo "<script>alert('Session Anda Telah Habis!!'); window.location = '$logout_redirect_url'</script>";
     }
 }
 $_SESSION['start_time'] = time();
